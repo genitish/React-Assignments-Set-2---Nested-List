4b736ae8a9a773062ac31d8e6e25417e4cbcd80c
Nitish <genitish@gmail.com>
2021-06-01 16:01:53 +0530

    changes update

--- src/components/List.js
+++ src/components/List.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 
 
 const List = ({states}) =>{
-
+    
     const [stateIndex, setState] = useState(null);
     const [cityIndex, setCity] = useState(null);
     const stateHandler = (index) =>{
-        if(stateIndex != null){
+        if(stateIndex != null && index === stateIndex){
             setState(null)
-            setCity(null)
         }
         else{
             setState(index)
         }
+        setCity(null)
     }
     const cityHandler = (index) =>{
-        if(cityIndex != null){
+        if(cityIndex != null && index === cityIndex){
             setCity(null)
         }
         else{
@@ -25,49 +25,31 @@ const List = ({states}) =>{
 
     return(
         <div className = " container">
-            <ol>States:
+            <ol>
                 {states.map((state,index) =>
                 <li id = {"state" + (index+1)} key = {index+1} >
                     <button onClick = {e => stateHandler(index)}>{state.name}</button>
 
-                     <ul>
                     {stateIndex != undefined && state.name == states[stateIndex].name
-                        && states[stateIndex].cities.map((city,index) =>
+                        && <ol>{states[stateIndex].cities.map((city,index) =>
                             <li id = {"city" + (index+1)} key = {index+1} >
                                 <button onClick = {e => cityHandler(index)}>{city.name} </button>
 
-                                <ul>
-                                    {   cityIndex!= undefined && 
-                                    city.name == states[stateIndex].cities[cityIndex].name && 
-                                    states[stateIndex].cities[cityIndex].towns.map((town,index) =>
-                                        <li id = {"town" + (index+1)} key = {index+1} >
-                                            {town.name}
+                                    {cityIndex!= undefined && 
+                                        city.name == states[stateIndex].cities[cityIndex].name && 
+                                        <ol>{states[stateIndex].cities[cityIndex].towns.map((town,index) =>
+                                            <li id = {"town" + (index+1)} key = {index+1} >
+                                                {town.name}
                                             </li>
-                                        )} 
-                                </ul>
+                                        )}</ol>
+                                    }
                             </li>
                         )}
-                    </ul>
+                        </ol>}
                      
                 </li>
                 )}
             </ol>
-
-            {/* <ol>City:
-                {stateIndex && states[stateIndex].cities.map((city,index) =>
-                <li id = {"city" + (index+1)} key = {index+1} >
-                    <button onClick = {e => cityHandler(index)}>{city.name}
-                    </button></li>
-                )}
-            </ol>
-
-            <ol>Towns:
-                {cityIndex && states[stateIndex].cities[cityIndex].towns.map((town,index) =>
-                <li id = {"town" + (index+1)} key = {index+1} >
-                    {town.name}
-                    </li>
-                )}
-                </ol> */}
 
         </div>
     )
